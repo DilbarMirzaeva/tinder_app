52b93bf065f5d960c968e4fdb5c05f7b83521501
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-06-03 01:16:31 +0400

    userService CRUD

--- src/main/java/az/turing/tinderdemo/domain/repository/UserRepo.java
+++ src/main/java/az/turing/tinderdemo/domain/repository/UserRepo.java
@@ -4,4 +4,5 @@ import az.turing.tinderdemo.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User,Long> {
+    boolean existsUserByUsername(String username);
 }
--- src/main/java/az/turing/tinderdemo/service/UserService.java
+++ src/main/java/az/turing/tinderdemo/service/UserService.java
@@ -1,4 +1,13 @@
 package az.turing.tinderdemo.service;
 
+import az.turing.tinderdemo.dto.request.UserCreateRequest;
+import az.turing.tinderdemo.dto.response.UserResponse;
+
+import java.util.List;
+
 public interface UserService {
+    UserResponse saveUser(UserCreateRequest user);
+    List<UserResponse> getAllUsers();
+    UserResponse getUserById(Long id);
+    void deleteUserById(Long id);
 }
