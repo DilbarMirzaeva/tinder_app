06f4402c766f6980b589d7267e2515796632d84d
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-09-24 20:37:11 +0400

    delete createMethod

--- src/main/java/az/turing/service/MatchService.java
+++ src/main/java/az/turing/service/MatchService.java
@@ -1,12 +1,10 @@
 package az.turing.service;
 
-import az.turing.domain.entity.User;
 import az.turing.dto.response.MatchResponse;
 
 import java.util.List;
 
 public interface MatchService {
-    void createMatch(User fromUser,User toUser);
     List<MatchResponse> getAllMatchesByUser(Long userId);
     boolean isMatched(Long userId1, Long userId2);
     void deleteMatch(Long matchId);
