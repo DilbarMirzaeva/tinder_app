b168873d055bf1cfcadce1f0cf369cf5dfcb1497
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-09-24 20:35:13 +0400

    add methods

--- src/main/java/az/turing/domain/repository/LikeRepo.java
+++ src/main/java/az/turing/domain/repository/LikeRepo.java
@@ -1,7 +1,9 @@
 package az.turing.domain.repository;
 
 import az.turing.domain.entity.Like;
+import az.turing.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepo extends JpaRepository<Like,Long> {
+    boolean existsByFromUserAndToUser(User fromUser, User toUser);
 }
--- src/main/java/az/turing/domain/repository/MatchRepo.java
+++ src/main/java/az/turing/domain/repository/MatchRepo.java
@@ -1,7 +1,12 @@
 package az.turing.domain.repository;
 
 import az.turing.domain.entity.Match;
+import az.turing.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MatchRepo extends JpaRepository<Match,Long> {
+    List<Match> findByUser1OrUser2(User fromUser, User toUser);
+    boolean existsByUser1AndUser2OrUser2AndUser1(User user1, User user2, User user2Alt, User user1Alt);
 }
